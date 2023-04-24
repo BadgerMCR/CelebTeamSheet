0a1a7ed88728ad7d27f864253129211aa94c1eae
Badger & Combes <95646286+BadgerMCR@users.noreply.github.com>
2023-04-24 12:46:44 +0100

    Add files via upload

--- CTeamSheet/index.js
+++ CTeamSheet/index.js
@@ -26,87 +26,172 @@ module.exports = {
 		},
 		{
 			name: "Crb",
-			description: "Right Back",
+			description: "Right Back Number",
+			type: "String"
+		},
+		{
+			name: "Crbname",
+			description: "Right Back Name",
 			type: "String"
 		},
 		{
 			name: "Ccb1",
-			description: "Centre Back 1",
+			description: "Centre Back 1 Number",
+			type: "String"
+		},
+		{
+			name: "Ccb1name",
+			description: "Centre Back 1 Name",
 			type: "String"
 		},
 		{
 			name: "Ccb2",
-			description: "Centre Back 2",
+			description: "Centre Back 2 Number",
+			type: "String"
+		},
+		{
+			name: "Ccb2name",
+			description: "Centre Back 2 Name",
 			type: "String"
 		},
 		{
 			name: "Ccb3",
-			description: "Centre Back 3",
+			description: "Centre Back 3 Number",
+			type: "String"
+		},
+		{
+			name: "Ccb3name",
+			description: "Centre Back 3 Name",
 			type: "String"
 		},
 		{
 			name: "Clb",
-			description: "Left Back",
+			description: "Left Back Number",
+			type: "String"
+		},
+		{
+			name: "Clbname",
+			description: "Left Back Name",
 			type: "String"
 		},
 		{
 			name: "Crm",
-			description: "Right Midfielder",
+			description: "Right Midfielder Number",
+			type: "String"
+		},
+		{
+			name: "Crmname",
+			description: "Right Midfielder Name",
 			type: "String"
 		},
 		{
 			name: "Ccdm",
-			description: "Centre Defensive Midfielder",
+			description: "Centre Defensive Midfielder Number",
+			type: "String"
+		},
+		{
+			name: "Ccdmname",
+			description: "Centre Defensive Midfielder Name",
 			type: "String"
 		},
 		{
 			name: "Ccm1",
-			description: "Centre Midfielder 1",
+			description: "Centre Midfielder 1 Number",
+			type: "String"
+		},
+		{
+			name: "Ccm1name",
+			description: "Centre Midfielder 1 Name",
 			type: "String"
 		},
 		{
 			name: "Ccm2",
-			description: "Centre Midfielder 2",
+			description: "Centre Midfielder 2 Number",
+			type: "String"
+		},
+		{
+			name: "Ccm2name",
+			description: "Centre Midfielder 2 Name",
 			type: "String"
 		},
 		{
 			name: "Ccm3",
-			description: "Centre Midfielder 3",
+			description: "Centre Midfielder 3 Number",
+			type: "String"
+		},
+		{
+			name: "Ccm3name",
+			description: "Centre Midfielder 3 Name",
 			type: "String"
 		},
 		{
 			name: "Ccam",
-			description: "Centre Attacking Midfielder",
+			description: "Centre Attacking Midfielder Number",
+			type: "String"
+		},
+		{
+			name: "Ccamname",
+			description: "Centre Attacking Midfielder Name",
 			type: "String"
 		},
 		{
 			name: "Clm",
-			description: "Left Midfielder",
+			description: "Left Midfielder Number",
+			type: "String"
+		},
+		{
+			name: "Clmname",
+			description: "Left Midfielder Name",
 			type: "String"
 		},
 		{
 			name: "Crw",
-			description: "Right Winger",
+			description: "Right Wing Number",
+			type: "String"
+		},
+		{
+			name: "Crwname",
+			description: "Right Wing Name",
 			type: "String"
 		},
 		{
 			name: "Cst1",
-			description: "Striker 1",
+			description: "Striker 1 Number",
+			type: "String"
+		},
+		{
+			name: "Cst1name",
+			description: "Striker 1 Name",
 			type: "String"
 		},
 		{
 			name: "Cst2",
-			description: "Striker 2",
+			description: "Striker 2 Number",
+			type: "String"
+		},
+		{
+			name: "Cst2name",
+			description: "Striker 2 Name",
 			type: "String"
 		},
 		{
 			name: "Cst3",
-			description: "Striker 3",
+			description: "Striker 3 Number",
+			type: "String"
+		},
+		{
+			name: "Cst3name",
+			description: "Striker 3 Name",
 			type: "String"
 		},
 		{
 			name: "Clw",
-			description: "Left Winger",
+			description: "Left Wing Number",
+			type: "String"
+		},
+		{
+			name: "Clwname",
+			description: "Left Wing Name",
 			type: "String"
 		},
 	],
